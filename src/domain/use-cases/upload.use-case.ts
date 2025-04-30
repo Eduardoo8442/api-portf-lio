fdf4568e11b86ca7664f58ead2e265c3578dad52
Eduardo <rocamrpm@gmail.com>
2025-04-30 00:50:40 -0300

    fixed erros in upload

--- src/domain/use-cases/upload.use-case.ts
+++ src/domain/use-cases/upload.use-case.ts
@@ -7,34 +7,34 @@ dotenv.config();
 class UploadImage {
   static async executeUseCase(request: FastifyRequest, reply: FastifyReply): Promise<void> {
     try {
-
       const file = await request.file();
-
       if (!file) {
         return reply.status(400).send('Nenhum arquivo foi enviado.');
       }
 
       const uploadPath = path.resolve(__dirname, '../../uploads');
       if (!fs.existsSync(uploadPath)) {
-        fs.mkdirSync(uploadPath, { recursive: true }); 
+        fs.mkdirSync(uploadPath, { recursive: true });
       }
 
-     
+
       const randomName = Array.from({ length: 20 }, () => Math.floor(Math.random() * 10)).join('');
       const fileExtension = path.extname(file.filename); 
       const filename = `${randomName}${fileExtension}`;
       const filepath = path.join(uploadPath, filename);
 
+
       const writeStream = fs.createWriteStream(filepath);
-      file.file.pipe(writeStream);
+
+
+      await new Promise<void>((resolve, reject) => {
+        file.file.pipe(writeStream)
+          .on('error', (err) => reject(err))
+          .on('finish', () => resolve()); 
+      });
 
       const imageLink = `${process.env.URL}/uploads/${filename}`;
       reply.status(200).send({ imageLink });
-
-      writeStream.on('error', (err) => {
-        console.error('Erro ao salvar o arquivo:', err);
-        reply.status(500).send('Erro ao processar upload.');
-      });
 
     } catch (error) {
       console.error('Erro ao processar upload:', error);
